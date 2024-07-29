8c135d18e91a02166a089de6bebb9561536bf069
kemi Chris <ekeminichristopher123@gmail.com>
2024-07-30 00:16:58 +0100

    Nav structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <div className='nav-bar'>
-        
+        <div className="nav-heading">
+          <img src="" alt="" />
+          <p>Ekemini Christopher</p>
+          <small>Frontend Developer</small>
+          <div className="nav-socials">
+            
+          </div>
+        </div>
     </div>
   )
 }
