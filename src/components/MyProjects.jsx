fc2cbae89089f14716d65aed78f3e4d018ab558c
kemi Chris <ekeminichristopher123@gmail.com>
2024-08-03 04:26:09 +0100

    Creating the Projects file

---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MyProjects = () => {
+  return (
+    <div className='my-projects'>
+        <h2>My Works</h2>
+        <div className="work-cards">
+            
+        </div>
+    </div>
+  )
+}
