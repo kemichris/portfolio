0d0b0951b9d014dc60c48142d07259e69d3764fe
kemi Chris <ekeminichristopher123@gmail.com>
2024-08-06 18:39:51 +0100

    finished Navbar styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import "./styles/Home.css";
 import { Navbar } from './components/Navbar'
-import {TopContainer} from "./components/TopContainer"
+import { TopContainer } from "./components/TopContainer"
 import { MyProjects } from './components/MyProjects'
 import { Skills } from './components/Skills'
 
 export const Home = () => {
   return (
     <div>
-      <Navbar />
-      <TopContainer />
-      <MyProjects/>
-      <Skills/>
+      <main>
+        <div className="main-flex">
+          <Navbar />
+          <div className="main-content">
+            <TopContainer />
+            <MyProjects />
+            <Skills />
+          </div>
+
+        </div>
+      </main>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../assets/logo.png"
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+  const [theme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme(!theme)
+  }
+
+
+
   return (
     <div className='nav-bar'>
-        <div className="nav-heading">
-          <img src={Logo} alt="" />
-          <p>Ekemini Christopher</p>
-          <small>Frontend Developer</small>
-          <div className="nav-socials">
-            <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
-            <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
-          </div>
+      <div className="nav-heading">
+        <img src={Logo} alt="" />
+        <p>Ekemini Christopher</p>
+        <small>Frontend Developer</small>
+        <div className="nav-socials">
+          <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+          <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="https://github.com/kemichris" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
         </div>
+      </div>
 
-        <div className="nav-content">
-          <Link to="/"><i class="fa-solid fa-house"></i> HomePage</Link>
-          <Link to="/about"><i class="fa-solid fa-user"></i> About</Link>
-          <Link to="/projects"><i class="fa-solid fa-briefcase"></i> Projects</Link>
-          <Link to="/contact"><i class="fa-solid fa-envelope"></i> Contact</Link>
-        </div>
+      <div className="nav-content">
 
-        <div className="theme-switcher">
-          <i class="fa-solid fa-moon"></i>
-          <i class="fa-solid fa-bahai"></i>
-        </div>
+        <Link className='link home' to="/"><i class="fa-solid fa-house icon"></i> HomePage</Link>
+        <Link className='link' to="/about"><i class="fa-solid fa-user"></i> About</Link>
+        <Link className='link' to="/projects"><i class="fa-solid fa-briefcase"></i> Projects</Link>
+        <Link className='link' to="/contact"><i class="fa-solid fa-envelope"></i> Contact</Link>
+      </div>
+
+      <div className="theme-switcher">
+        <button onClick={toggleTheme}> {theme ? <i class="fa-solid fa-moon"></i> : <i class="fa-solid fa-bahai"></i>}</button>
+
+
+      </div>
 
 
     </div>
