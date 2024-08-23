4e070bcc02ec4129ea933191246d14ff57a6e37f
kemi Chris <ekeminichristopher123@gmail.com>
2024-08-23 18:20:39 +0100

    skills page styles

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const SkillsCard = (props) => {
     const {languageImg, languageName} = props;
-    
+
   return (
-    <div>
+    <div className='skills-card'>
         <img src={languageImg} alt="" />
         <h4>{languageName}</h4>
     </div>
