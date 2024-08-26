17f5ee62bbe5ec77eae9ef41bed9ae4215ae9156
kemi Chris <ekeminichristopher123@gmail.com>
2024-08-26 03:44:51 +0100

    call to action section

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const CallToAction = () => {
+  return (
+    <div className='call-to-action'>
+        <h2>Whats Next?</h2>
+        <div className="action-text">
+          <p>Do you need a beautiful, responsive and fully functional website? Let's work together! 
+            Click the button below and let's bring your dreams to reality.
+          </p>
+          <Link to="/contact">
+            <button>Get in touch</button>
+          </Link>
+        </div>
+    </div>
+  )
+}
